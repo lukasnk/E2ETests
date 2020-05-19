ba0d1f46bafabbd0400dd4dc1da2b06fd04d2e97
lukasnk <lukasznaurecki@gmail.com>
2020-05-19 15:37:01 +0200

    add download test

---
+++ src/main/java/Pages/AutomationPracticeFormPage.java
@@ -0,0 +1,27 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.IOException;
+
+public class AutomationPracticeFormPage {
+
+    static String link = "a[href'http://toolsqa.com/wp-content/uploads/2016/09/Test-File-to-Download.xlsx']";
+    static StringBuilder wgetCommand = new StringBuilder().append("wget ").append(link);
+
+
+    public static void DownloadLink(WebDriver driver){
+
+        try {
+            Process exec = Runtime.getRuntime().exec(String.valueOf(wgetCommand));
+            int exitVal = exec.waitFor();
+            System.out.println("Exit value: " + exitVal);
+        } catch (InterruptedException | IOException ex) {
+            System.out.println(ex.toString());
+        }
+        driver.close();
+    }
+}
